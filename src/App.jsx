6ddb1550b5dd79487d6de0cf9bc73499086af4d5
Hardeep Singh <105967614+hrdps@users.noreply.github.com>
2024-07-16 13:39:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+// import Home_ from './components/home/Home_.jsx';
+import Home_ from './components/home/Hometwo.jsx';
+
+function App() {
+  return (
+    <div>
+      <Home_ />
+    </div>
+  );
+}
+
+export default App;
